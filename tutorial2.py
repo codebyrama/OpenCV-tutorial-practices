56c90b58000cf8a2f2ba0951f56f2144999ffab1
codebyrama <ikramahossain@iut-dhaka.edu>
2023-09-28 17:13:51 +0600

    22

--- tutorial2.py
+++ tutorial2.py
@@ -1,10 +1,11 @@
+# sourcery skip: use-itertools-product
 import cv2
 import random
-img=cv2.imread("F:\Learning Coding\OpenCV_tutorial\OpenCV-tutorial-practices\logo.jpg",-1)
+img=cv2.imread("F:/Learning Coding/OpenCV_tutorial/OpenCV-tutorial-practices/logo.jpg",-1)
 print(img)
 print(type(img))
 print(img.shape) #rows,collumn, channel, 3 values representing the value
-for i in range(100):
+for i in range(100): 
     for j in range(100):
         img[i][j]=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
 tag=img[320:860,10:1885]
